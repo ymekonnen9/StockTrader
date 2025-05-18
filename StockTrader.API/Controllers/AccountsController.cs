3add7ea7433f30152e701bf61f88a4512d189cf9
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-05-17 20:55:57 -0500

    added a Register DTO and an endpoint to register users

---
+++ StockTrader.API/Controllers/AccountsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StockTrader.Application.DTOs;
+using StockTrader.Domain.Entities;
+
+namespace StockTrader.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _usermanager;
+        private readonly RoleManager<IdentityRole> _rolemanager;
+    
+        public AccountsController(UserManager<ApplicationUser> usermanager, RoleManager<IdentityRole> rolemanager)
+        {
+            _usermanager = usermanager;
+            _rolemanager = rolemanager;
+        }
+
+        [HttpPost("/Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userEmail = await _usermanager.FindByEmailAsync(registerDto.Email);
+
+            if(userEmail != null)
+            {
+                return BadRequest(new { message = "The user email is already registered" });
+            }
+
+            var userName = await _usermanager.FindByNameAsync(registerDto.UserName);
+
+            if(userName != null)
+            {
+                return BadRequest(new { message = "The username is already taken. Create a unique username" });
+            }
+            ApplicationUser newUser = new ApplicationUser
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+
+            };
+
+            IdentityResult result = await _usermanager.CreateAsync(newUser, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                var defaultRole = "User";
+
+                if(await _rolemanager.RoleExistsAsync(defaultRole))
+                {
+                    await _usermanager.AddToRoleAsync(newUser, defaultRole);
+                } else
+                {
+                    Console.WriteLine("Role doesn't exist");
+                }
+
+                return Ok(new { message = "User Created successfully" });
+
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+
+        } 
+    
+    }
+}
---
+++ StockTrader.Application/DTOs/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.Application.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters.")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
