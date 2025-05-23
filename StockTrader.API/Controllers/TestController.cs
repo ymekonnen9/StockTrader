1ccfb057b9cad96ba5f49f5133ded651d71f8d51
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-05-23 18:32:21 -0500

    added a testcontroller

---
+++ StockTrader.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿// In StockTrader.API/Controllers/TestController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockTrader.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok(new { message = "Pong from TestController!" });
+        }
+    }
+}
